c379b5182c1e46253483783d5aea28329f05d58c
Parker Bleything <54328065+bleytp6889@users.noreply.github.com>
2019-10-03 23:56:35 -0400

    Update HelloTest1.java
    added required comments on lines 3 & 9

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/3/19 by PB
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * My first Pull Request!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
